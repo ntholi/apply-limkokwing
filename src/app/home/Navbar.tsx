fa7430d6836ab51a21d57085e8b223d0093942b7
Ntholi Nkhatho <ntholi@live.com>
2024-01-16 17:06:48 +0200

    init Navbar

---
+++ src/app/home/Navbar.tsx
@@ -0,0 +1,106 @@
+'use client';
+import React from 'react';
+import {
+  Navbar as NextUiNavbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from '@nextui-org/react';
+import Image from 'next/image';
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    {
+      name: 'Home',
+      href: '#',
+    },
+    {
+      name: 'Courses',
+      href: '#',
+    },
+    {
+      name: 'Contact',
+      href: '#',
+    },
+  ];
+
+  return (
+    <NextUiNavbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent className='sm:hidden' justify='start'>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        />
+      </NavbarContent>
+
+      <NavbarContent className='sm:hidden pr-3' justify='center'>
+        <NavbarBrand>
+          <Image
+            src={'/images/logo.png'}
+            className='w-full h-16'
+            height={100}
+            width={100}
+            priority={false}
+            alt='Logo'
+          />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+        <NavbarBrand>
+          <Image
+            src={'/images/logo.png'}
+            className='w-full h-16'
+            height={100}
+            width={100}
+            priority={false}
+            alt='Logo'
+          />
+        </NavbarBrand>
+        {menuItems.map((item) => (
+          <NavbarItem key={item.name}>
+            <Link color='foreground' href={item.href}>
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent justify='end'>
+        <NavbarItem className='hidden lg:flex'>
+          <Link href='#'>Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color='default' href='#' variant='flat'>
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className='w-full'
+              href={item.href}
+              size='lg'
+              color='foreground'
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </NextUiNavbar>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
+import Navbar from './home/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang='en' className='dark'>
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   );
