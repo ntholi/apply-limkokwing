2168b8cb8c9144cc16346bb980c78aa8006af623
Ntholi Nkhatho <ntholi@live.com>
2024-01-16 17:47:58 +0200

    Add Lucide React icon and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import { Button } from '@nextui-org/react';
 import Container from './core/Container';
+import { GraduationCap } from 'lucide-react';
 
 export default function Home() {
   return (
-    <Container as='main'>
-      <div className='flex justify-center mt-10 flex-col items-center gap-3'>
-        <h1 className='text-5xl'>Hello World</h1>
-        <Button color='primary' className='w-full block sm:w-52' radius='full'>
-          Apply Now
-        </Button>
+    <Container
+      as='main'
+      className='flex justify-center mt-10 flex-col items-center gap-3'
+    >
+      <div className='text-sm text-center p-2 rounded-lg border border-foreground-500 flex gap-2 items-center'>
+        <GraduationCap />
+        <h1>Limkokwing University of Creative Technology</h1>
       </div>
+      <h2 className='md:text-8xl text-center font-bold uppercase text-5xl'>
+        Be The Most Successful
+      </h2>
+      <Button
+        color='primary'
+        className='w-full block sm:w-52 mt-20'
+        radius='full'
+      >
+        Apply Now
+      </Button>
     </Container>
   );
 }
