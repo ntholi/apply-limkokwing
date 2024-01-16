1068d5338515b3b6052181644aa4b2a7128023ad
Ntholi Nkhatho <ntholi@live.com>
2024-01-16 14:02:53 +0200

    setup nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react';
+
 export default function Home() {
   return (
     <main>
-      <h1 className='p-5 bg-gray-900 text-white text-6xl text-center mt-10'>
-        Limkokwing
-      </h1>
+      <div className='flex justify-center mt-10'>
+        <Button color='primary'>Click Me</Button>
+      </div>
     </main>
   );
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,10 @@
+// app/providers.tsx
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react';
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
+
+export default Providers;
