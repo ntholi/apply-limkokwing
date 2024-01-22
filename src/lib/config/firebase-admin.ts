70dfdbd6cdf7852f4a5a0d0491b199360d01d879
Ntholi Nkhatho <ntholi@live.com>
2024-01-22 20:45:52 +0200

    Update firebase-admin configuration

--- src/lib/config/firebase-admin.ts
+++ src/lib/config/firebase-admin.ts
@@ -1,19 +1,23 @@
 import { cert, getApp, getApps, ServiceAccount } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
 
-function createFirebaseAdminApp() {
+const credentials: ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    : undefined,
+};
+
+function adminApp() {
   if (getApps().length === 0) {
     return admin.initializeApp({
-      credential: cert(serviceAccount as admin.ServiceAccount),
+      credential: cert(credentials),
     });
   } else {
     return getApp();
   }
 }
 
-const app = createFirebaseAdminApp();
-const adminAuth = getAuth(app);
-
-export { adminAuth };
+export const adminAuth = getAuth(adminApp());
