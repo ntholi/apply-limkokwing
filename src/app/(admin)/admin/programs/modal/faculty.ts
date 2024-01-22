a84b73b5d2ea8b70ed6400b83757c7711b8545ae
Ntholi Nkhatho <ntholi@live.com>
2024-01-22 19:20:13 +0200

    Remove unused faculties from the list

--- src/app/(admin)/admin/programs/modal/faculty.ts
+++ src/app/(admin)/admin/programs/modal/faculty.ts
@@ -1,7 +1,6 @@
 export const Faculties = [
   { code: 'FAID', name: 'Faculty of Architecture and Interior Design' },
   { code: 'FBS', name: 'Faculty of Business and Globalisation	Programs' },
-  { code: 'FCM', name: 'Faculty of Creative Multimedia	Programs' },
   { code: 'FCO', name: 'Faculty of Communication and Media	Programs' },
   {
     code: 'FCTH',
@@ -9,12 +8,10 @@ export const Faculties = [
   },
   { code: 'FDSI', name: 'Faculty of Design and Innovation	Programs' },
   { code: 'FFLD', name: 'Faculty of Fashion and Lifestyle Design	Programs' },
-  { code: 'FFTB', name: 'Faculty of Film, Television & Broadcasting	Programs' },
   {
     code: 'FINT',
     name: 'Faculty of Information & Communication Technology	Programs',
   },
-  { code: 'FMS', name: 'Faculty of Music and Sound	Programs' },
 ] as const;
 
 export type Faculty = (typeof Faculties)[number];
