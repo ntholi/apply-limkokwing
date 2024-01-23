4e8b330c26024a5ba7c1f7326616bf7a9787c84d
Ntholi Nkhatho <ntholi@live.com>
2024-01-23 10:21:57 +0200

    Add accordion and document list to Review component

--- src/app/(main)/apply/review/Review.tsx
+++ src/app/(main)/apply/review/Review.tsx
@@ -2,17 +2,56 @@ import React from 'react';
 import { ContentWrapper } from '../page';
 import { useApplication } from '../ApplicationProvider';
 import ResultsTable from './ResultsTable';
+import {
+  Accordion,
+  AccordionItem,
+  Button,
+  CardHeader,
+  Link,
+} from '@nextui-org/react';
+import { IconCheck } from '@tabler/icons-react';
 
 export default function Review() {
   const application = useApplication();
   if (!application) return null;
 
   return (
-    <div>
-      <ContentWrapper>
-        <h2>Qualifications</h2>
-        <ResultsTable application={application} />
-      </ContentWrapper>
-    </div>
+    <ContentWrapper>
+      <Accordion>
+        <AccordionItem key='1' title='Qualifications'>
+          <h2 className='p-1 font-semibold'>{application.certificate.name}</h2>
+          <ResultsTable application={application} />
+        </AccordionItem>
+        <AccordionItem key='2' title='Courses'>
+          x
+        </AccordionItem>
+        <AccordionItem key='3' title='Documents'>
+          <ul className='flex flex-col gap-2'>
+            {application.documents.map((doc) => (
+              <li key={doc.name} className='flex gap-3 items-center'>
+                <Button
+                  isIconOnly
+                  size='sm'
+                  radius='full'
+                  color='success'
+                  variant='bordered'
+                  className='border-1'
+                >
+                  <IconCheck size={'1rem'} />
+                </Button>
+                <Link
+                  href={doc.url}
+                  showAnchorIcon
+                  target='_blank'
+                  color='foreground'
+                >
+                  {doc.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </AccordionItem>
+      </Accordion>
+    </ContentWrapper>
   );
 }
