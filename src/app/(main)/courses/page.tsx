b43474fe3bf8bc2cd7a41693f608a4e6a3ad7d1e
Ntholi Nkhatho <ntholi@live.com>
2024-01-29 15:21:13 +0200

    Add prerequisites to CourseCard component

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -74,6 +74,12 @@ function CourseCard({ program }: { program: Program }) {
       <Divider />
       <CardBody className='text-small'>
         <p>Minimum of {program.requiredCredits} credits required</p>
+        <p>
+          And must have a credit in:{' '}
+          <span className='text-zinc-400'>
+            {program.prerequisites.map((it) => `${it.courseName} `)}
+          </span>
+        </p>
       </CardBody>
     </Card>
   );
