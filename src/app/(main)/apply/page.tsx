759c77a846c0228403d275e0827defe21ef39abb
Ntholi Nkhatho <ntholi@live.com>
2024-01-29 11:57:15 +0200

    Refactor navigation buttons and container styles

--- src/app/(main)/apply/page.tsx
+++ src/app/(main)/apply/page.tsx
@@ -68,33 +68,35 @@ export default function StartPage() {
         <Stepper className='my-8 sm:my-10' />
         <div className='mt-5 flex flex-col gap-5'>
           <div className='pb-10'>{steps[step - 1]}</div>
-          <div className='fixed bottom-0 left-0 right-0 backdrop-blur-sm bg-gradient-to-r from-black/10 border-t border-zinc-950 to-black'>
-            <nav className='flex justify-between max-w-screen-lg container mx-auto px-4 py-5'>
-              <Button
-                isDisabled={step === 1}
-                onClick={() => {
-                  if (step > 1) {
-                    setStep(step - 1);
-                  }
-                }}
-              >
-                Back
-              </Button>
-              <Button
-                color='primary'
-                isDisabled={!canProceed}
-                onClick={() => {
-                  if (step < steps.length) {
-                    setStep(step + 1);
-                  } else {
-                    handleSubmit();
-                  }
-                }}
-                isLoading={isPending}
-              >
-                {step === steps.length ? 'Submit' : 'Next'}
-              </Button>
-            </nav>
+          <div className='fixed bottom-0 left-0 right-0 md:max-w-screen-lg md:container md:mx-auto md:px-4 md:-mb-2'>
+            <div className='md:px-6 backdrop-blur-sm bg-gradient-to-r md:rounded-2xl from-black/10 border border-gray-900 to-black'>
+              <nav className='flex justify-between max-w-screen-lg container mx-auto px-4 py-5'>
+                <Button
+                  isDisabled={step === 1}
+                  onClick={() => {
+                    if (step > 1) {
+                      setStep(step - 1);
+                    }
+                  }}
+                >
+                  Back
+                </Button>
+                <Button
+                  color='primary'
+                  isDisabled={!canProceed}
+                  onClick={() => {
+                    if (step < steps.length) {
+                      setStep(step + 1);
+                    } else {
+                      handleSubmit();
+                    }
+                  }}
+                  isLoading={isPending}
+                >
+                  {step === steps.length ? 'Submit' : 'Next'}
+                </Button>
+              </nav>
+            </div>
           </div>
         </div>
       </Container>
