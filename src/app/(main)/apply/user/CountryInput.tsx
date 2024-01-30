cc826380b1bc0adbda0875e56dffb9755753d3b3
Ntholi Nkhatho <ntholi@live.com>
2024-01-30 17:10:28 +0200

    Refactor CountryInput component to improve code readability and add missing functionality

--- src/app/(main)/apply/user/CountryInput.tsx
+++ src/app/(main)/apply/user/CountryInput.tsx
@@ -1,12 +1,18 @@
 import { AutocompleteProps, Input, InputProps } from '@nextui-org/react';
-import React from 'react';
-import { continents, countries, languages } from 'countries-list';
+import React, { Key } from 'react';
+import { TCountryCode, continents, countries, languages } from 'countries-list';
 import ReactCountryFlag from 'react-country-flag';
 import {
   Autocomplete,
   AutocompleteSection,
   AutocompleteItem,
 } from '@nextui-org/react';
+import {
+  getCountryCode,
+  getCountryData,
+  getCountryDataList,
+  getEmojiFlag,
+} from 'countries-list';
 
 type Props = {
   value: string;
@@ -14,22 +20,30 @@ type Props = {
 };
 
 export default function CountryInput({ value, onChange, ...rest }: Props) {
-  const data = Object.entries(countries).map(([code, country]) => ({
-    label: country.name,
-    value: country.name,
+  const data = Object.entries(countries).map(([_, country]) => ({
+    name: country.name,
+    value: country,
   }));
+
   return (
     <>
-      {/* <ReactCountryFlag countryCode='US' svg /> */}
       <Autocomplete
         label='Country'
         defaultItems={data}
         value={value}
-        onSelectionChange={(item) => onChange(item.toString())}
+        onSelectionChange={(key) => onChange(key.toString())}
         selectedKey={value}
       >
         {(item) => (
-          <AutocompleteItem key={item.value}>{item.label}</AutocompleteItem>
+          <AutocompleteItem key={item.name}>
+            {/* <span className='flex items-center gap-2'>
+              <ReactCountryFlag
+                countryCode={getCountryCode(item.name).toString()}
+                svg
+              /> */}
+            {item.name}
+            {/* </span> */}
+          </AutocompleteItem>
         )}
       </Autocomplete>
     </>
