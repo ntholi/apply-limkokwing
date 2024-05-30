1e00dbae8e0b69e388f52e4821197abc533d0716
Ntholi Nkhatho <ntholi@live.com>
2024-05-30 14:45:16 +0200

    refactor: update background color and border in Home component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       as='main'
       className='flex justify-center mt-4 flex-col items-center gap-3'
     >
-      <div className='text-center p-3 rounded bg-zinc-900 border border-foreground-500 flex gap-2 items-center'>
+      <div className='text-center p-3 rounded bg-zinc-900 border border-zinc-700 flex gap-2 items-center'>
         <h1 className='text-xs sm:text-sm'>
           Limkokwing University of Creative Technology
         </h1>
