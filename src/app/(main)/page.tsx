338bafb0fdf6704a83e9449a1f9e6c404710ee12
Ntholi Nkhatho <ntholi@live.com>
2024-05-30 14:43:47 +0200

    refactor: update page layout and styles in Home component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,20 @@
 'use client';
 
 import Container from './core/Container';
-import { GraduationCap } from 'lucide-react';
 import HomeButton from './home/HomeButton';
 
 export default function Home() {
   return (
     <Container
       as='main'
-      className='flex justify-center pt-10 flex-col items-center gap-3'
+      className='flex justify-center mt-4 flex-col items-center gap-3'
     >
-      <div className='text-center p-2 rounded-lg border border-foreground-500 flex gap-2 items-center'>
-        <GraduationCap />
+      <div className='text-center p-3 rounded bg-zinc-900 border border-foreground-500 flex gap-2 items-center'>
         <h1 className='text-xs sm:text-sm'>
           Limkokwing University of Creative Technology
         </h1>
       </div>
-      <h2 className='md:text-8xl text-center font-bold uppercase text-5xl'>
+      <h2 className='md:text-8xl text-center font-bold uppercase text-5xl mt-6'>
         Be The Most Successful
       </h2>
       <HomeButton />
