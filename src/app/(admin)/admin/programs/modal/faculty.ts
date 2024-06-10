913efa9eb35e8e8ff9c963e48be898afdeea9233
Ntholi Nkhatho <ntholi@live.com>
2024-06-10 16:59:09 +0200

    refactor: Update faculty codes and names in faculty.ts

--- src/app/(admin)/admin/programs/modal/faculty.ts
+++ src/app/(admin)/admin/programs/modal/faculty.ts
@@ -1,15 +1,15 @@
 export const Faculties = [
-  { code: 'FABE', name: 'Faculty of Architecture and Interior Design' },
-  { code: 'FBMG', name: 'Faculty of Business and Globalisation	Programs' },
+  { code: 'FABE', name: 'Faculty of Architecture and Build Environment' },
+  { code: 'FBMG', name: 'Faculty of Business and Globalisation' },
   { code: 'FCMB', name: 'Faculty of Communication, Media & Broadcasting' },
   {
     code: 'FCTH',
-    name: 'Faculty of Creativity in Tourism & Hospitality	Programs',
+    name: 'Faculty of Creativity in Tourism & Hospitality',
   },
-  { code: 'FDI', name: 'Faculty of Design and Innovation	Programs' },
+  { code: 'FDI', name: 'Faculty of Design and Innovation' },
   {
     code: 'FICT',
-    name: 'Faculty of Information & Communication Technology	Programs',
+    name: 'Faculty of Information & Communication Technology',
   },
 ] as const;
 
