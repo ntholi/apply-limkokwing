17b0590a38fe81d84883c9590d202389d7f34ef4
Ntholi Nkhatho <ntholi@live.com>
2024-09-07 19:35:45 +0200

    Delete everyting

--- src/app/(admin)/admin-core/DetailsView.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from '@mantine/core';
-import React, { PropsWithChildren } from 'react';
-
-export default function DetailsView({ children }: PropsWithChildren) {
-  return <Stack p={40}>{children}</Stack>;
-}
--- src/app/(admin)/admin/applications/modals/Results.ts
+++
@@ -1,6 +0,0 @@
-import { GradingScheme } from '../../certificates/Certificate';
-
-export interface Results {
-  course: string;
-  grade: GradingScheme;
-}
