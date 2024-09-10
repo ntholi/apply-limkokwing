4d0f8283717d819a746ab6a0e190cc7ed2e85841
Ntholi Nkhatho <ntholi@live.com>
2024-09-10 13:41:46 +0200

    remove TanStackRouterDevtools

--- packages/client/src/routes/__root.tsx
+++ packages/client/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { ModeToggle } from '@/components/theme/mode-toggle';
 import { ThemeProvider } from '@/components/theme/theme-provider';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
@@ -10,7 +9,6 @@ export const Route = createRootRoute({
       <div className='fixed bottom-0 right-0 p-4'>
         <ModeToggle />
       </div>
-      <TanStackRouterDevtools />
     </ThemeProvider>
   ),
 });
