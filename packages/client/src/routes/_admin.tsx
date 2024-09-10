0c89e869722ad135318a3fe237110447804613fd
Ntholi Nkhatho <ntholi@live.com>
2024-09-10 15:56:11 +0200

    Add generic account information to account panel

--- packages/client/src/routes/_admin.tsx
+++ packages/client/src/routes/_admin.tsx
@@ -82,15 +82,12 @@ function AdminLayout() {
           <div className='mt-auto p-4'>
             <Card x-chunk='dashboard-02-chunk-0'>
               <CardHeader className='p-2 pt-0 md:p-4'>
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
+                <CardTitle>Account</CardTitle>
+                <CardDescription>Your Account Info</CardDescription>
               </CardHeader>
               <CardContent className='p-2 pt-0 md:p-4 md:pt-0'>
                 <Button size='sm' className='w-full'>
-                  Upgrade
+                  Logout
                 </Button>
               </CardContent>
             </Card>
