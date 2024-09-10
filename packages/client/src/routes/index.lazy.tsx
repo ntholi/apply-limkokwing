e449d72ee02ed8b7ffa24ddcfb57d24dbdbffb24
Ntholi Nkhatho <ntholi@live.com>
2024-09-10 11:48:59 +0200

    add content to index page

--- packages/client/src/routes/index.lazy.tsx
+++ packages/client/src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
 import Body from '@/components/base/Body';
-import { createLazyFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
+import { createLazyFileRoute, Link } from '@tanstack/react-router';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -7,8 +8,16 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <Body>
-      <h3></h3>
+    <Body className='flex flex-col items-center mt-12 gap-6 sm:gap-12'>
+      <h1 className='text-sm border py-3 rounded-sm sm:w-[25%] text-center'>
+        Limkokwing University of Creative Technology
+      </h1>
+      <p className='text-8xl font-bold w-1/2 text-center'>
+        Be The Most Successful
+      </p>
+      <Button className='w-full sm:w-[25%]' asChild>
+        <Link to={'/apply'}>Apply Now</Link>
+      </Button>
     </Body>
   );
 }
