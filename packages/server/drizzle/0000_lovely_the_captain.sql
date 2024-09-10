d049919cbae88581a206605bfb026922742f2fad
Ntholi Nkhatho <ntholi@live.com>
2024-09-10 16:35:13 +0200

    setup schema

--- packages/server/drizzle/0000_lovely_the_captain.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "user" (
-	"id" serial NOT NULL,
-	"name" text,
-	"email" text,
-	"password" text,
-	"role" text DEFAULT 'user',
-	"created_at" timestamp,
-	"updated_at" timestamp
-);
--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -1,11 +1,45 @@
-import { serial, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
+import {
+  serial,
+  text,
+  timestamp,
+  pgTable,
+  integer,
+  boolean,
+  uuid,
+} from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
-  id: serial('id'),
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().notNull().defaultRandom(),
   name: text('name'),
   email: text('email'),
   password: text('password'),
   role: text('role').$type<'user' | 'admin'>().default('user'),
-  createdAt: timestamp('created_at'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at'),
+});
+
+export const courses = pgTable('courses', {
+  id: serial('id').primaryKey().notNull(),
+  name: text('name'),
+  minCredits: integer('min_credits'),
+  minPasses: integer('min_passes'),
+  description: text('description'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at'),
+});
+
+export const prerequisites = pgTable('prerequisites', {
+  id: serial('id').primaryKey().notNull(),
+  courseId: integer('course_id').references(() => courses.id),
+  subjectId: integer('subject_id').references(() => subjects.id),
+  isMandatory: boolean('is_mandatory').default(false),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at'),
+});
+
+export const subjects = pgTable('subjects', {
+  id: serial('id').primaryKey().notNull(),
+  name: text('name'),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at'),
 });
