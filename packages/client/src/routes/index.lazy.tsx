3cc49fb501392bd7f9d33eedb803d707fbce3c0f
Ntholi Nkhatho <ntholi@live.com>
2024-09-10 11:51:37 +0200

    Make landing page responsive

--- packages/client/src/routes/index.lazy.tsx
+++ packages/client/src/routes/index.lazy.tsx
@@ -8,14 +8,14 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <Body className='flex flex-col items-center mt-12 gap-6 sm:gap-12'>
-      <h1 className='text-sm border py-3 rounded-sm sm:w-[25%] text-center'>
+    <Body className='flex flex-col items-center mt-12 gap-12'>
+      <h1 className='text-sm border py-3 px-2 rounded-sm md:w-[25%] text-center w-full'>
         Limkokwing University of Creative Technology
       </h1>
-      <p className='text-8xl font-bold w-1/2 text-center'>
+      <p className='text-5xl sm:text-6xl md:text-8xl font-bold md:w-1/2 text-center'>
         Be The Most Successful
       </p>
-      <Button className='w-full sm:w-[25%]' asChild>
+      <Button className='w-full md:w-[25%]' asChild>
         <Link to={'/apply'}>Apply Now</Link>
       </Button>
     </Body>
